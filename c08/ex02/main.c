db62e392c6d60d0a53818ff8df9c58cf7d966ee0
ZenVega <ufogreeks@gmail.com>
2024-10-07 11:41:24 +0200

    add c08/ex04

---
+++ c08/ex02/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ft_abs.h"
+int main()
+{
+  int x;
+  int y;
+
+  x = ABS(3);
+  printf("absolutes value of x = %d\n", x);
+  y = ABS(-1);
+  printf("absolute value of y = %d\n", y);
+  return (0);
+}
